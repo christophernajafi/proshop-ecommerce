519cb848e7594970748ea89eaef657a099282145
Christopher Najafi <chris.najafi@gmail.com>
2020-10-04 18:35:27 -0400

    Added database seeder, route controllers, and error-handling middleware.

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -14,17 +16,14 @@ connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GET");
+  res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
