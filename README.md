4797ad679906bc34ebe0ec935cbe2a2b84a154ec
Christopher Najafi <chris.najafi@gmail.com>
2020-10-11 22:05:02 -0400

    Added Morgan, updated dependencies, and prepared server for deployment.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ProShop eCommerce Platform
+
+## Environmental Variables
+
+```shell
+NODE_ENV=development
+PORT=5000
+MONGO_URI=
+JWT_SECRET=
+PAYPAL_CLIENT_ID=
+```
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
@@ -16,9 +17,9 @@ connectDB();
 
 const app = express();
 
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
@@ -35,10 +36,10 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
